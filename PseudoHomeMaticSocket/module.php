9d42ffbc46022ad86dd4a578204b18db39d9a8e3
TobiasWW <tobiww@outlook.de>
2016-12-27 20:43:17 +0100

    Typos

--- PseudoHomeMaticSocket/module.php
+++ PseudoHomeMaticSocket/module.php
@@ -1,24 +1,24 @@
 <?
     class PseudoHomeMaticSocket extends IPSModule
-	{
+    {
         public function __construct($InstanceID)
-		{
+        {
             parent::__construct($InstanceID);
         }
 
         public function Create()
-		{
+        {
             parent::Create();
-		}
+        }
 
         public function ApplyChanges()
-		{
+        {
             parent::ApplyChanges();
         }
-		
-		public function ForwardData($JSONString)
-		{
-			IPS_LogMessage(__CLASS__ . "." . __FUNCTION__, $JSONString);
-		}
+
+        public function ForwardData($JSONString)
+        {
+            IPS_LogMessage(__CLASS__ . "." . __FUNCTION__, $JSONString);
+        }
     }
 ?>
